2bacf0a2cf094ed4cda941c9bdb4ee61d44a7daa
ruslanmelnic <ruslan.melnic@amdaris.com>
2023-10-31 12:21:06 +0200

    refactoring code, adding functions

--- app.js
+++ app.js
@@ -6,35 +6,40 @@ let num2 = num + 11
 
 console.log(num2)
 
-const resultElement = document.getElementById( 'result')
-const input1 = document.getElementById( 'input1')
-const input2 = document. getElementById( 'input2')
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
 const submitBtn = document.getElementById('submit')
 const minusBTN = document.getElementById('minus')
 const plusBTN = document.getElementById('plus')
 
-// console.log(resultElement. textContent)
-// resultElement. textContent = (42 - 2) / num
-// console.log(typeof sum)
-minusBTN.onclick = function (){
+function printResult(result) {
+    if (result < 0) {
+        resultElement.style.color = 'red'
+    } else {
+        resultElement.style.color = 'green'
+    }
+    resultElement.textContent = result
+}
+
+minusBTN.onclick = function () {
     action = '-'
 }
 
-plusBTN.onclick = function (){
+plusBTN.onclick = function () {
     action = '+'
 }
 
-submitBtn.onclick = function () {
-    if (action === '+'){
-        const sum = Number(input1. value) + Number(input2.value)
-        resultElement. textContent = sum
-    }
-    else if (action === '-'){
-        const sum = Number(input1. value) - Number(input2.value)
-        resultElement. textContent = sum
-    }
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    return actionSymbol === '+' ? num1 + num2 : num1 - num2
 }
 
-//test
+submitBtn.onclick = function () {
+    const result = computeNumbersWithAction(input1, input2, action)
+    printResult(result)
+}
+//test-tes
 
 
