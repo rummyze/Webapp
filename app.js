67709c7542ffd58e3bf83e75a929586b3015212f
ruslanmelnic <ruslan.melnic@amdaris.com>
2023-10-31 19:56:23 +0200

    refactoring code, adding functions

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ let num2 = num + 11
 
 console.log(num2)
 
-const resultElement = document.getElementById( 'result')
-const input1 = document.getElementById( 'input1')
-const input2 = document. getElementById( 'input2')
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
 const submitBtn = document.getElementById('submit')
 const minusBTN = document.getElementById('minus')
 const plusBTN = document.getElementById('plus')
@@ -16,23 +16,31 @@ const plusBTN = document.getElementById('plus')
 // console.log(resultElement. textContent)
 // resultElement. textContent = (42 - 2) / num
 // console.log(typeof sum)
-minusBTN.onclick = function (){
+minusBTN.onclick = function () {
     action = '-'
 }
 
-plusBTN.onclick = function (){
+plusBTN.onclick = function () {
     action = '+'
+}
+function printResult(result){
+    if (result < 0 ){
+        resultElement.style.color = 'red'
+    }else {
+        resultElement.style.color = 'green'
+    }
+    resultElement.textContent = result
+}
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    return actionSymbol == '+' ? num1 + num2 : num1 - num2
 }
 
 submitBtn.onclick = function () {
-    if (action === '+'){
-        const sum = Number(input1. value) + Number(input2.value)
-        resultElement. textContent = sum
-    }
-    else if (action === '-'){
-        const sum = Number(input1. value) - Number(input2.value)
-        resultElement. textContent = sum
-    }
+    const result = computeNumbersWithAction(input1, input2, action)
+    printResult(result)
 }
 
 //test
