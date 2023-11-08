a21a75936bec998e8ff6548d11d8e805f35f2cc4
ruslanmelnic <ruslan.melnic@amdaris.com>
2023-11-08 14:44:23 +0200

    feature: adding loadNotes and saveNotes to store locally notes

--- 02/app.js
+++ 02/app.js
@@ -1,48 +1,52 @@
+// Получение элементов интерфейса
 const inputElement = document.getElementById('title');
 const createBtn = document.getElementById('create');
 const listElement = document.getElementById('list');
-const notes = [
-    {
-        title: 'TestName',
-        stateOfComplications: false,
-    },
-    {
-        title: 'NameTest1',
-        stateOfComplications: true,
-    }
-];
 
+// Загрузка заметок или установка начального значения
+function loadNotes() {
+    const savedNotes = localStorage.getItem('notes');
+    return savedNotes ? JSON.parse(savedNotes) : [];
+}
+
+// Сохранение заметок в localStorage
+function saveNotes() {
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+// Отображение заметок на странице
 function render() {
     listElement.innerHTML = '';
-    if (notes.length === 0) {
-        listElement.innerHTML = '<p>Add notes</p>';
-    }
     notes.forEach((note, i) => {
         listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note, i));
     });
 }
 
+// Создание HTML-шаблона заметки
 function getNoteTemplate(note, index) {
     return `<li class="list-group-item d-flex justify-content-between align-items-center">
-      <span class="${note.stateOfComplications ? 'text-decoration-line-through' : ''}"
-            data-index="${index}"
-            data-type="edit">${note.title}</span>
-      <span>
-        <button class="btn btn-small btn-${note.stateOfComplications ? 'warning' : 'success'}"
-                data-index="${index}"
-                data-type="toggle">&#10003;</button>
-        <button class="btn btn-small btn-danger"
-                data-index="${index}"
-                data-type="remove">&#10005;</button>
-      </span>
+        <span class="${note.stateOfComplications ? 'text-decoration-line-through' : ''}"
+              data-index="${index}"
+              data-type="edit">${note.title}</span>
+        <span>
+            <button class="btn btn-small btn-${note.stateOfComplications ? 'warning' : 'success'}"
+                    data-index="${index}"
+                    data-type="toggle">&#10003;</button>
+            <button class="btn btn-small btn-danger"
+                    data-index="${index}"
+                    data-type="remove">&#10005;</button>
+        </span>
     </li>`;
 }
 
+// Изменение заголовка заметки
 function saveTitleEdit(index, value) {
     notes[index].title = value;
+    saveNotes();
     render();
 }
 
+// Обработка кликов на заметках
 listElement.onclick = function (event) {
     const index = parseInt(event.target.dataset.index, 10);
     const type = event.target.dataset.type;
@@ -50,45 +54,48 @@ listElement.onclick = function (event) {
     if (!isNaN(index)) {
         if (type === 'toggle') {
             notes[index].stateOfComplications = !notes[index].stateOfComplications;
+            saveNotes();
             render();
         } else if (type === 'remove') {
             notes.splice(index, 1);
+            saveNotes();
             render();
-        }
-    }
+        } else if (type === 'edit') {
+            const note = notes[index];
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = note.title;
+            input.classList.add('form-control');
+            event.target.parentNode.replaceChild(input, event.target);
+            input.focus();
 
-    if (type === 'edit') {
-        const note = notes[index];
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = note.title;
-        input.classList.add('form-control');
-
-        event.target.parentNode.replaceChild(input, event.target);
-        input.focus();
-
-        input.onblur = function() {
-            saveTitleEdit(index, input.value);
-        };
-        input.onkeypress = function(e) {
-            if (e.key === 'Enter') {
+            input.onblur = function () {
                 saveTitleEdit(index, input.value);
-            }
-        };
+            };
+            input.addEventListener('keydown', function (e) {
+                if (e.key === 'Enter') {
+                    saveTitleEdit(index, input.value);
+                    e.preventDefault();
+                }
+            });
+        }
     }
 };
 
+// Создание новой заметки
 createBtn.onclick = function () {
     if (inputElement.value.length === 0) {
         return;
     }
     const newNote = {
-        title: inputElement.value,
-        stateOfComplications: false
+        title: inputElement.value, stateOfComplications: false
     };
     notes.push(newNote);
+    saveNotes();
     render();
     inputElement.value = '';
 };
 
+// Инициализация приложения
+const notes = loadNotes();
 render();
