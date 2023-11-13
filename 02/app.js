1305dede762a09bab45f44347196627472ae94f7
ruslanmelnic <ruslan.melnic@amdaris.com>
2023-11-13 11:58:19 +0200

    fixing variable

--- 02/app.js
+++ 02/app.js
@@ -1,39 +1,64 @@
+// Получение элементов интерфейса
 const inputElement = document.getElementById('title');
 const createBtn = document.getElementById('create');
 const listElement = document.getElementById('list');
-const sortOptions = document.getElementById('sortOptions'); // Получаем элемент для сортировки
+const sortOptions = document.getElementById('sortOptions');
+const filterValue = document.querySelector('input[name="filter"]:checked').value;
 
 // Загрузка заметок или установка начального значения
 let notes = loadNotes();
 render();
 
-// Загрузка заметок или установка начального значения
+// Функция загрузки заметок
 function loadNotes() {
     const savedNotes = localStorage.getItem('notes');
     return savedNotes ? JSON.parse(savedNotes) : [];
 }
 
-// Сохранение заметок в localStorage
+// Функция сохранения заметок
 function saveNotes() {
     localStorage.setItem('notes', JSON.stringify(notes));
 }
 
-
-// Отображение заметок на странице
 function render() {
-    listElement.innerHTML = '';
-    notes.forEach((note, i) => {
-        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note, i));
+    listElement.innerHTML = ''; // Очистка списка перед добавлением новых элементов
+    let filteredNotes = filterNotes(filterValue, notes);
+    filteredNotes.forEach((note, index) => {
+        const noteElement = document.createElement('li');
+        noteElement.innerHTML = getNoteTemplate(note, index);
+        noteElement.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center', 'note-item');
+        // Добавляем элемент в DOM
+        listElement.appendChild(noteElement);
+        // Теперь добавляем класс для анимации появления
+        setTimeout(() => {
+            noteElement.classList.add('show');
+        }, 10); // небольшая задержка, чтобы CSS успел отреагировать
     });
 }
 
-// Сортировка заметок
-sortOptions.addEventListener('change', function (e) {
-    const sortOption = e.target.value;
+
+// Функция фильтрации заметок
+function filterNotes(filterValue, notes) {
+    if (filterValue === 'all') {
+        return notes;
+    }
+    const isCompleted = filterValue === 'completed';
+    return notes.filter(note => note.stateOfComplications === isCompleted);
+}
+
+// События фильтрации
+document.querySelectorAll('input[name="filter"]').forEach(input => {
+    input.addEventListener('change', render);
+});
+
+// Событие сортировки
+sortOptions.addEventListener('change', function () {
+    const sortOption = this.value;
     sortNotes(sortOption);
     render();
 });
 
+// Функция сортировки заметок
 function sortNotes(sortOption) {
     if (sortOption === 'title') {
         notes.sort((a, b) => a.title.localeCompare(b.title));
@@ -44,9 +69,9 @@ function sortNotes(sortOption) {
     }
 }
 
-// Создание HTML-шаблона заметки
+// Функция для создания HTML-шаблона заметки
 function getNoteTemplate(note, index) {
-    return `<li class="list-group-item d-flex justify-content-between align-items-center">
+    return `
         <span class="${note.stateOfComplications ? 'text-decoration-line-through' : ''}"
               data-index="${index}"
               data-type="edit">${note.title}</span>
@@ -57,64 +82,72 @@ function getNoteTemplate(note, index) {
             <button class="btn btn-small btn-danger"
                     data-index="${index}"
                     data-type="remove">&#10005;</button>
-        </span>
-    </li>`;
+        </span>`;
 }
 
-// Изменение заголовка заметки
+// Создание новой заметки
+createBtn.addEventListener('click', function () {
+    const title = inputElement.value.trim();
+    if (title) {
+        const newNote = {
+            title, stateOfComplications: false, createdAt: new Date().toISOString()
+        };
+        notes.push(newNote);
+        saveNotes();
+        render();
+        inputElement.value = '';
+    }
+});
+
+// Обработка кликов на заметках
+listElement.addEventListener('click', function (event) {
+    const index = parseInt(event.target.dataset.index, 10);
+    const type = event.target.dataset.type;
+    if (isNaN(index)) return;
+    switch (type) {
+        case 'toggle':
+            toggleCompletion(index)
+            break;
+        case'remove':
+            removeNote(index);
+            break;
+        case'edit':
+            editNoteTitle(event, index);
+            break;
+    }
+});
+
+function toggleCompletion(index) {
+    notes[index].stateOfComplications = !notes[index].stateOfComplications;
+    saveNotes();
+    render();
+}
+
+function removeNote(index) {
+    notes.splice(index, 1);
+    saveNotes();
+    render();
+}
+
+function editNoteTitle(event, index) {
+    const note = notes[index];
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = note.title;
+    input.classList.add('form-control');
+    event.target.parentNode.replaceChild(input, event.target);
+    input.focus();
+
+    input.onblur = () => saveTitleEdit(index, input.value);
+    input.onkeydown = (e) => {
+        if (e.key === 'Enter') {
+            saveTitleEdit(index, input.value);
+        }
+    };
+}
+
 function saveTitleEdit(index, value) {
     notes[index].title = value;
     saveNotes();
     render();
 }
-
-// Обработка кликов на заметках
-listElement.onclick = function (event) {
-    const index = parseInt(event.target.dataset.index, 10);
-    const type = event.target.dataset.type;
-
-    if (!isNaN(index)) {
-        if (type === 'toggle') {
-            notes[index].stateOfComplications = !notes[index].stateOfComplications;
-            saveNotes();
-            render();
-        } else if (type === 'remove') {
-            notes.splice(index, 1);
-            saveNotes();
-            render();
-        } else if (type === 'edit') {
-            const note = notes[index];
-            const input = document.createElement('input');
-            input.type = 'text';
-            input.value = note.title;
-            input.classList.add('form-control');
-            event.target.parentNode.replaceChild(input, event.target);
-            input.focus();
-
-            input.onblur = function () {
-                saveTitleEdit(index, input.value);
-            };
-            input.addEventListener('keydown', function (e) {
-                if (e.key === 'Enter') {
-                    saveTitleEdit(index, input.value);
-                    e.preventDefault();
-                }
-            });
-        }
-    }
-};
-
-// Создание новой заметки
-createBtn.onclick = function () {
-    if (inputElement.value.length === 0) {
-        return;
-    }
-    const newNote = {
-        title: inputElement.value, stateOfComplications: false, createdAt: new Date().toISOString()
-    };
-    notes.push(newNote);
-    saveNotes();
-    render();
-    inputElement.value = '';
-};
-
